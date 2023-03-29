952abf5fe22fc341ad0d89a4f57d9498c4235bbf
Crypto-kiki <ejo2123@naver.com>
2023-03-29 14:21:42 +0900

    random data complete

--- main.js
+++ main.js
@@ -5,11 +5,21 @@ function Generate() {
 
   const CardName = document.querySelector(".CardName");
   const introduce = document.querySelector(".introduce");
+  const Rarity = document.querySelector(".Rarity");
+  const Attack = document.querySelector(".Attack");
+  const Hp = document.querySelector(".Hp");
+  const Element = document.querySelector(".Element");
 
   fetch(`https://meta-data.syltare.com/${RandomNumber}/info.json`)
     .then((response) => response.json())
     .then((data) => {
       CardName.innerText = data.name;
       introduce.innerText = data.description;
+
+      // data.attrivutes의 객체에서 value값 접근
+      Rarity.innerText = data.attributes[0].value;
+      Attack.innerText = data.attributes[1].value;
+      Hp.innerText = data.attributes[2].value;
+      Element.innerText = data.attributes[6].value;
     });
 }
